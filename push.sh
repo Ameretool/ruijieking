1a04cd186931fd8372e8bd6714c72e83856b77ce
ruijieking <baywvwubeuege873@gmail.com>
2026-07-27 15:05:46 +0800

    更新remote仓库上传脚本

---
+++ push.sh
@@ -0,0 +1,6 @@
+**  一键上传GitHub脚本，第一次启动会让你填写name和email，和绑定运程仓库最后填写GitHub密钥下次再启动就可以直接上传了
+**  再需要上传的根目录里面输入./push.sh
+**  "  "那里自己修改上传更新内容
+git add .
+git commit -m "更新remote仓库上传脚本"
+git push origin main
